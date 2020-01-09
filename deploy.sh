0916dd734a44c12c3b7be56886ac3172f5732387
lgq186 <1043736808@qq.com>
2020-01-09 18:16:17 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,7 @@ git commit -m 'deploy'
 # 如果你想要部署到 https://USERNAME.github.io
 git push -f git@github.com:liguiqinxxn/liguiqinxxn.github.io.git master
 
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "我的个人网站",
   head: [
     // 注入到当前页面的 HTML <head> 中的标签
-    ["link", { rel: "icon", href: "/logo.ico" }] // 增加一个自定义的 favicon(网页标签的图标)
+    ["link", { rel: "icon", href: `/logo.ico` }] // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: "/", // 这是部署到github相关的配置
   markdown: {
@@ -11,20 +11,68 @@ module.exports = {
   },
   themeConfig: {
     nav: [
-      { text: "主页", link: "/" },
+      { text: "首页", link: "/" },
       // { text: "接口定义", link: "/apiword.md" },
-      { text: "接口字段定义", link: "/api.html" }
-      // { text: "附录：错误码", link: "/error.md" }
+      { text: "前端基础", link: "/api.html" },
+      // 下拉列表的配置
+      {
+        text: "Languages",
+        items: [
+          { text: "Chinese", link: "/language/chinese" },
+          { text: "English", link: "/language/English" }
+        ]
+      }
+    ],
+    sidebar: [
+      {
+        title: "前端",
+        collapsable: false,
+        children: [
+          ["/foo/one", "one"],
+          ["/foo/two", "two"]
+        ]
+      },
+      {
+        title: "后端",
+        collapsable: false,
+        children: [
+          ["/bar/three", "three"],
+          ["/bar/four", "four"]
+        ]
+      }
     ],
     // sidebar: {
-    //   "/": [
-    //     "/", //指的是根目录的md文件 也就是 README.md 里面的内容
-    //     "apiword", // 根目录创建 apiword.md文件
-    //     "api", // 根目录创建 api.md文件
-    //     "error" // 根目录创建 error.md文件
+    //   // 侧边栏在 /foo/ 上
+    //   "/foo/": ["", "/one.html", "/two"]
+    //   // 侧边栏在 /bar/ 上
+    //   "/bar/": ["", "three", "four"]
+    // },
+    // sidebar: {
+    //   "/notes/frontEnd/": [
+    //     {
+    //       title: "前端",
+    //       collapsable: false,
+    //       children: [
+    //         "/VueJS组件编码规范.md",
+    //         "/notes/frontEnd/vue-cli脚手架快速搭建项目.md",
+    //         "/notes/frontEnd/深入理解vue中的slot与slot-scope",
+    //         "/notes/frontEnd/webpack入门",
+    //         "/notes/frontEnd/PWA介绍及快速上手搭建一个PWA应用"
+    //       ]
+    //     }
+    //   ],
+    //   "/notes/backEnd/": [
+    //     {
+    //       title: "后端",
+    //       collapsable: true,
+    //       children: [
+    //         "/notes/backEnd/nginx入门",
+    //         "/notes/backEnd/CentOS如何挂载磁盘"
+    //       ]
+    //     }
     //   ]
     // },
-    sidebar: "auto", // 侧边栏配置
+    // sidebar: "auto", // 侧边栏配置
     sidebarDepth: 2 // 侧边栏显示2级
   }
 };
