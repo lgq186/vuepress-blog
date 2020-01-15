3f2de364a74df8e5201a9e70ed906546b8706332
lgq186 <1043736808@qq.com>
2020-01-15 14:37:44 +0800

    更新博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,54 +24,59 @@ module.exports = {
       }
     ],
     sidebar: [
+      // {
+      //   title: "前端",
+      //   collapsable: false,
+      //   children: [
+      //     ["/foo/one", "one"],
+      //     ["/foo/two", "two"]
+      //   ]
+      // },
+      // {
+      //   title: "后端",
+      //   collapsable: false,
+      //   children: [
+      //     ["/bar/three", "three"],
+      //     ["/bar/four", "four"]
+      //   ]
+      // },
       {
-        title: "前端",
+        title: "面试题",
         collapsable: false,
         children: [
-          ["/foo/one", "one"],
-          ["/foo/two", "two"]
-        ]
-      },
-      {
-        title: "后端",
-        collapsable: false,
-        children: [
-          ["/bar/three", "three"],
-          ["/bar/four", "four"]
+          {
+            title: "HTML",
+            collapsable: true,
+            children: [["/interview/HTML/HTML面试", "HTML"]]
+          },
+          {
+            title: "CSS",
+            collapsable: true,
+            children: [["/interview/CSS/CSS面试", "CSS"]]
+          },
+          {
+            title: "JS",
+            collapsable: true,
+            children: [["/interview/JS/JS面试", "JS"]]
+          },
+          {
+            title: "ES6",
+            collapsable: true,
+            children: [["/interview/ES6/ES6面试", "ES6"]]
+          },
+          {
+            title: "VUE",
+            collapsable: true,
+            children: [
+              [
+                "/interview/vue/vue路由跳转四种方式",
+                "vue 路由跳转四种方式 (带参数)"
+              ]
+            ]
+          }
         ]
       }
     ],
-    // sidebar: {
-    //   // 侧边栏在 /foo/ 上
-    //   "/foo/": ["", "/one.html", "/two"]
-    //   // 侧边栏在 /bar/ 上
-    //   "/bar/": ["", "three", "four"]
-    // },
-    // sidebar: {
-    //   "/notes/frontEnd/": [
-    //     {
-    //       title: "前端",
-    //       collapsable: false,
-    //       children: [
-    //         "/VueJS组件编码规范.md",
-    //         "/notes/frontEnd/vue-cli脚手架快速搭建项目.md",
-    //         "/notes/frontEnd/深入理解vue中的slot与slot-scope",
-    //         "/notes/frontEnd/webpack入门",
-    //         "/notes/frontEnd/PWA介绍及快速上手搭建一个PWA应用"
-    //       ]
-    //     }
-    //   ],
-    //   "/notes/backEnd/": [
-    //     {
-    //       title: "后端",
-    //       collapsable: true,
-    //       children: [
-    //         "/notes/backEnd/nginx入门",
-    //         "/notes/backEnd/CentOS如何挂载磁盘"
-    //       ]
-    //     }
-    //   ]
-    // },
     // sidebar: "auto", // 侧边栏配置
     sidebarDepth: 2 // 侧边栏显示2级
   }
